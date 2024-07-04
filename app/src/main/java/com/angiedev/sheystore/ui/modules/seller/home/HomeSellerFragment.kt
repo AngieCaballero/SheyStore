d01e6fcc06ccfd1a243da2a33731efe0798c4a51
AngieCaballero <karma2000angiejaz@gmail.com>
2024-07-03 21:33:39 -0600

    task: upgrade dependencies version

--- app/src/main/java/com/angiedev/sheystore/ui/modules/seller/home/HomeSellerFragment.kt
+++ app/src/main/java/com/angiedev/sheystore/ui/modules/seller/home/HomeSellerFragment.kt
@@ -1,6 +1,5 @@
 package com.angiedev.sheystore.ui.modules.seller.home
 
-import androidx.fragment.app.Fragment
 import com.angiedev.sheystore.databinding.FragmentHomeSellerBinding
 import com.angiedev.sheystore.ui.base.BaseFragment
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
-    id("com.google.dagger.hilt.android") version "2.51" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version "1.6.0-M1" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
